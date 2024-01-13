1f5f2ae7597c74295585ab88f1c209e13cf48c08
Dmytro Cheglakov <dcheglakov@gmail.com>
2024-01-13 22:39:06 +0200

    feat: letters logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,24 +1,51 @@
 import React from "react";
 
-import { sample } from "../../utils";
+import { sample, range } from "../../utils";
 import { WORDS } from "../../data";
 import GuessForm from "../GuessForm";
 import Guess from "../Guess";
+import { NUM_OF_GUESSES_ALLOWED, NUM_LETTERS } from "../../constants";
 
 // Pick a random word on every page load.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
-function Game() {
-  const [guesses, setGuesses] = React.useState();
+const initialGuesses = range(0, NUM_OF_GUESSES_ALLOWED).map(() => {
+  const guess = range(0, NUM_LETTERS).map(() => ({
+    id: crypto.randomUUID(),
+    letter: " ",
+    match: false,
+  }));
+  return [...guess];
+});
 
+function Game() {
+  const [guesses, setGuesses] = React.useState(initialGuesses);
+  console.log({ guesses });
   return (
     <>
       <Guess guesses={guesses} />
       <GuessForm
         onGuessSubmit={(guess) => {
-          setGuesses((prevGuesses) => console.log(guess));
+          const newGuess = guess.split("").map((letter, index) => ({
+            id: crypto.randomUUID(),
+            letter,
+            status:
+              answer[index] === letter
+                ? "correct"
+                : answer.includes(letter)
+                ? "misplaced"
+                : "incorrect",
+          }));
+          setGuesses((prevGuesses) => {
+            const newGuesses = [...prevGuesses];
+            const emptyGuessIndex = newGuesses.findIndex((guess) =>
+              guess.every((word) => word.letter === " ")
+            );
+            newGuesses[emptyGuessIndex] = newGuess;
+            return newGuesses;
+          });
         }}
       />
     </>
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { range } from "../../utils";
-import { NUM_OF_GUESSES_ALLOWED, NUM_LETTERS } from "../../constants";
 
-const initialGuesses = range(0, NUM_OF_GUESSES_ALLOWED);
-
-function Guess({ guesses = initialGuesses }) {
+function Guess({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((result) => (
+      {guesses.map((guess) => (
         <p className="guess" key={crypto.randomUUID()}>
-          {range(0, NUM_LETTERS).map((i) => (
-            <span className="cell" key={crypto.randomUUID()}>
-              {" "}
+          {guess.map(({ id, letter, status }) => (
+            <span key={id} className={`cell ${status}`}>
+              {letter}
             </span>
           ))}
         </p>
